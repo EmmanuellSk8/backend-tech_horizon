76d078d6ade7a765a96549ac793a407d44cde34d
emmanuellsk8 <emmanuel50354@gmail.com>
2025-02-28 12:55:10 -0500

    backend added

---
+++ src/main/java/com/techHorizon/tech/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.techHorizon.tech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://tech-horizon02.netlify.app") // Cambia esto por la URL de tu frontend en producción
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
